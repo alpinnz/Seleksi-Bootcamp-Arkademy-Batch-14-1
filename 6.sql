55a033064078d9b5481d527fd9431af77190ed8b
Alpinnz <alpinnz@gmail.com>
2019-12-28 15:49:47 +0700

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,5 +1,7 @@
+-- Buat database
 CREATE DATABASE shop;
 
+-- Buat Table Product berserta atributnya
 CREATE TABLE Product(
     id int PRIMARY KEY NOT NULL,
     name varchar(25) NOT NULL,
@@ -8,34 +10,54 @@ CREATE TABLE Product(
     id_cashier int NOT NULL
 );
 
+-- Buat Table Category berserta atributnya
 CREATE TABLE Category(
     id int PRIMARY KEY NOT NULL,
     name varchar(25) NOT NULL
 );
 
+-- Buat Table Cashier berserta atributnya
 CREATE TABLE Cashier(
     id int PRIMARY KEY NOT NULL,
     name varchar(25) NOT NULL
 );
 
+-- menambahkan Foregnkey
 ALTER TABLE Product ADD FOREIGN KEY (id_category) REFERENCES Category(id);
 ALTER TABLE Product ADD FOREIGN KEY (id_cashier) REFERENCES Cashier(id);
 
+-- menambahnakn data (table yang relasi dengan forenky haris terisi dahulu)
 INSERT INTO Category (id, name) 
 VALUES 
 ('1', 'Food'),
 ('2', 'Drink');
 
+SELECT * FROM Category;
+
+-- menambahnakn data (table yang relasi dengan forenky haris terisi dahulu)
 INSERT INTO Cashier (id, name) 
 VALUES 
 ('1', 'Pevita Pearce'),
 ('2', 'Raisa Andriana'),
 ('3', 'Alfin NoviAji');
 
+SELECT * FROM Cashier;
+
+-- menambahnakn data
 INSERT INTO Product (id, name, price, email, id_category, id_cashier) 
 VALUES 
 ('1', 'Latte', '10000', '2', '1'),
 ('2', 'Cake', '20000', '1', '2'),
 ('3', 'Puding', '45000', '1','3');
 
+SELECT * FROM Product;
+
+-- Buatlah query untuk menghasilkan tampilan seperti ini :
+Cashier.name AS cashier, 
+Product.name AS product, 
+Category.name AS category, 
+Product.price AS price
+FROM Product 
+INNER JOIN Category ON Product.id_category = Category.id 
+INNER JOIN Cashier ON Product.id_cashier = Cashier.id ;
 
