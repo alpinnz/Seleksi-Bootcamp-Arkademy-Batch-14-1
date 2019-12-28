89009ff1fc0c4951cfbee46384ece00c53ee1e63
Alpinnz <alpinnz@gmail.com>
2019-12-28 10:55:18 +0700

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,23 @@
+
+const is_name_valid = (name) => {
+    let Regex = /^[A-Z]{3,}$/;
+    return Regex.test(name);
+}
+
+const is_age_valid = (age) => {
+    let Regex = /^[0-9]{2,2}$/;
+    return Regex.test(age);
+}
+
+// name
+console.log(is_name_valid('aa') ? "benar" : "salah");
+console.log(is_name_valid('aasd') ? "benar" : "salah");
+console.log(is_name_valid('AS') ? "benar" : "salah");
+console.log(is_name_valid('ABS') ? "benar" : "salah");
+console.log(is_name_valid('ASDWDW') ? "benar" : "salah");
+
+// age
+console.log(is_age_valid('aas') ? "benar" : "salah");
+console.log(is_age_valid('1') ? "benar" : "salah");
+console.log(is_age_valid('22') ? "benar" : "salah");
+console.log(is_age_valid('3123') ? "benar" : "salah");
